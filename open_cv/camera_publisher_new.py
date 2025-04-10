42ae228202bc359be70e15a16b59608396ef9e5e
LiamMcConnell5 <liammcconnell5@gmail.com>
2025-04-10 12:21:28 -0400

    publihsers

---
+++ open_cv/camera_publisher_new.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import rospy
+from sensor_msgs.msg import CompressedImage
+from cv_bridge import CvBridge
+import cv2
+
+publisherNodeName = "camera_sensor_publisher"
+topicName = "video_topic/compressed"
+
+rospy.init_node(publisherNodeName, anonymous=True)
+publisher = rospy.Publisher(topicName, CompressedImage, queue_size=1)
+rate = rospy.Rate(10) # Reduce FPS to 10 for stability
+
+videoCaptureObject = cv2.VideoCapture(1)
+bridgeObject = CvBridge()
+
+while not rospy.is_shutdown():
+	ret, frame = videoCaptureObject.read()
+	if ret:
+		rospy.loginfo("Video frame captured and published")
+
+		# Reduce resolution (e.g., 640x480)
+		frame = cv2.resize(frame, (640, 480))
+
+		# Convert to compressed image format
+		compressedImage = bridgeObject.cv2_to_compressed_imgmsg(frame, dst_format="jpeg")
+
+		# Publish compressed image
+		publisher.publish(compressedImage)
+
+	rate.sleep()
