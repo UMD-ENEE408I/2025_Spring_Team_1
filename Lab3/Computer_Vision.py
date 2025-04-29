f9dd4e4cc41a8af06d0c4f240ccaaa6dbc8bd5fb
LiamMcConnell5 <liammcconnell5@gmail.com>
2025-04-29 11:25:15 -0400

    line following turns + person detection wip

--- Lab3/Computer_Vision.py
+++ Lab3/Computer_Vision.py
@@ -6,42 +6,44 @@ cam = cv2.VideoCapture(0)  # Open webcam
 model = YOLO("yolo-Weights/yolov8n.pt")
 
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-              "teddy bear", "hair drier", "toothbrush"
-              ]
+			  "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+			  "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+			  "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+			  "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+			  "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+			  "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+			  "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+			  "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+			  "teddy bear", "hair drier", "toothbrush"
+			  ]
 
 while cam.isOpened():
-    ret, frame = cam.read()
-    results = model(frame, stream=True)
+	ret, frame = cam.read()
+	results = model(frame, stream=True)
 
 
-    if not ret:
-        break
+	if not ret:
+		break
 
-    for object in results:
-        boxes = object.boxes
-        
-        for box in boxes:
-            if (classNames[int(box.cls[0])] == "apple" or classNames[int(box.cls[0])] == "sports ball"):
-                x1,y1,x2,y2 = box.xyxy[0]
-                #cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 255), 3)
-                cv2.circle(frame, (int((x1+x2)/2), int((y1+y2)/2)), int((x2-x1)/2), (255, 0, 255), 3)
+	for object in results:
+		boxes = object.boxes
+		
+		for box in boxes:
+			x1,y1,x2,y2 = box.xyxy[0]
+			if (classNames[int(box.cls[0])] == "apple" or classNames[int(box.cls[0])] == "sports ball"):
+				#cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 255), 3)
+				cv2.circle(frame, (int((x1+x2)/2), int((y1+y2)/2)), int((x2-x1)/2), (255, 0, 255), 3)
 
-                #cv2.putText(frame, classNames[int(box.cls[0])], (int(x1), int(y1)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-                #cv2.putText(frame, str(round(box.conf[0].numpy() * 100, 2)) + "%", (int(x2)- 200, int(y1)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+				#cv2.putText(frame, classNames[int(box.cls[0])], (int(x1), int(y1)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+				#cv2.putText(frame, str(round(box.conf[0].numpy() * 100, 2)) + "%", (int(x2)- 200, int(y1)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+			elif (classNames[int(box.cls[0])] == "person"):
+				cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 255), 3)
 
-        
-    cv2.imshow("Yolo", frame)
+		
+	cv2.imshow("Yolo", frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
 
 cam.release()
 cv2.destroyAllWindows()
--- line.py
+++ line.py
@@ -1,9 +1,11 @@
 import cv2
 import numpy as np
 cap = cv2.VideoCapture(0)
+prev_message = "I don't see the line"
 
 while True:
     ret, frame = cap.read()
+    #frame = frame[60:120, 0:160]
     imgray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     ret, binary_image = cv2.threshold(imgray, 200, 255, cv2.THRESH_BINARY)
     contours, hierarchy = cv2.findContours(binary_image, 1, cv2.CHAIN_APPROX_NONE)
@@ -15,15 +17,24 @@ while True:
             cy = int(M['m01']/M['m00'])
             print("CX : "+str(cx)+"  CY : "+str(cy))
             if cx >= 400 :
-                print("Turn Right")
+                prev_message = "Turn Right" # actual motor code + assign var
             if cx < 400 and cx > 240 :
-                print("On Track!")
+                prev_message = "On Track!" # actual motor code + assign var
             if cx <=240 :
-                print("Turn Left")
+                prev_message = "Turn Left" # actual motor code + assign var
+            print(prev_message)
             cv2.circle(frame, (cx,cy), 5, (255,0,0), -1)
             cv2.drawContours(frame, c, -1, (0,255,0), 1)
     else :
-        print("I don't see the line")
+        # if line stops getting detected, turn unti l
+        if prev_message == "Turn Right":
+            print(prev_message) # actual motor code
+        if prev_message == "On Track!":
+            print(prev_message) # actual motor code
+        if prev_message == "Turn Left":
+            print(prev_message) # actual motor code
+        else:
+            print(prev_message)
     cv2.imshow("Mask",binary_image)
     cv2.imshow("Frame",frame)
     if cv2.waitKey(1) & 0xff == ord('q'):   # 1 is the time in ms
